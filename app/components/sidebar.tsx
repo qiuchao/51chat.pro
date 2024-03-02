e998bfc9d708246eeeffdd9a9f3afbcb1d857832
qiuchao <qc_hao@163.com>
2024-03-02 14:25:47 +0800

    no message

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          My ChatGPT
+          51chat.pro
         </div>
         <div className={styles["sidebar-sub-title"]}>
           你的个人 AI 助手
